e78234421442336af4aca18a026a7088132a8230
Your Name <you@example.com>
2021-11-22 14:08:29 +0100

    empezar

--- carreras/carreras.js
+++ carreras/carreras.js
@@ -1,19 +1,21 @@
 class Vehicle {
     velocity = 0
     maxVelocity = 500
+
     accelerate() {
-        if (this.velocity >= (this.maxVelocity - 10)) {
-            this.velocity += 10
-        } else {
+        this.velocity >= (this.maxVelocity - 10) ?  
+            this.velocity += 10 :  
             this.velocity = this.maxVelocity
-        }
-        
     }
+
     brake() {
-        if (this.velocity >= 10) {
-            this.velocity -= 10
-        } else {
+        this.velocity >= 10 ?
+            this.velocity -= 10 :  
             this.velocity = 0
-        }
     }
+ 
+    standing() {
+
+    }
+
 }
